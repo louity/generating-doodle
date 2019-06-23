bdd29f6f55e4af44c6dd78cffc3aac5d3ade2455
Thomas Kerdreux <thomaskerdreux@gmail.com>
2019-06-23 17:21:55 +0200

    testing condition and uncondition is working

--- test.py
+++ test.py
@@ -1,6 +1,7 @@
 '''
 File testing all functionalities
 '''
+import numpy as np
 import pickle
 import torch
 
@@ -25,27 +26,32 @@ def try_training(use_cuda):
 
 def try_generating(use_cuda, sigma,
                    uncondition=False,
-                   nbr_image=10):
+                   idx_image=10):
     '''
     uncondition is True: generate a cat from a random vector
     uncondition is False: generate a cat from an initial image of cat
     '''
-    hp_path = 'draw_models/hp_folder/cat_20000.pickle'
-    with open(hp_path, 'rb') as handle:
-        hp = pickle.load(handle)
+    # hp_path = 'draw_models/hp_folder/cat_20000.pickle'
+    # with open(hp_path, 'rb') as handle:
+    #    hp = pickle.load(handle)
+    hp = HParams()
     hp.use_cuda = use_cuda
+    hp.max_len_out = 300 # TODO: to change!!
+    # dataLoader associated to the data set
+    dataloader = DataLoader('data/cat.npz', hp)
     # load model
     model = Model(hyper_parameters=hp, parametrization='point')
     encoder_name = 'draw_models/encoder_cat_20000.pth'
     decoder_name = 'draw_models/decoder_cat_20000.pth'
 
     model.load(encoder_name, decoder_name)
-    print('printing image we will regenerate')
-    # TODO: print image corresponding to 10
-    print('printing image re-generated')
-
+    if not uncondition:
+        print('printing image we will regenerate')
+        dataloader.plot_image(idx_image, plot=True)
+    print('printing generated image')
+    model.dataloader = dataloader
     model.conditional_generation_point(10,
-                                       uncondition=True,
+                                       uncondition=uncondition,
                                        plot=True,
                                        sigma=sigma)
     print('done generating from initial image with sigma = {}'.format(sigma))
@@ -56,8 +62,9 @@ def try_completing(use_cuda, sigma):
 
 
 if __name__ == '__main__':
-    # try_training(False)
-    try_generating(False, 0.01)
+    try_training(False)
+    try_generating(False, 0.01, uncondition=False)
+    try_generating(False, 0.1, uncondition=True)
     if torch.cuda.is_available():
         try_training(True)
         try_completing(True, 0.01)
